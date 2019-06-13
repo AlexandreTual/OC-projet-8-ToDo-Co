81150e03bddd83c07c99f76d38eb9b44933b095b
Tual alexandre <tual.alexandre@gmail.com>
2019-06-13 15:48:22 +0200

    code cleaning / indentation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('default/index.html.twig');
     }
