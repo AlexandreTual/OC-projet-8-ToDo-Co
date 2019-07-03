fc265b8dc6ff3cdb82810afc5980e88f18ba9e34
Tual alexandre <tual.alexandre@gmail.com>
2019-07-03 11:50:22 +0200

    ignore test functionnal in travis

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace tests\Controller;
 
+use App\Entity\Task;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -22,20 +23,6 @@ class TaskControllerTest extends WebTestCase
     protected function setUp()
     {
         $this->client = self::createClient();
-    }
-
-    public function testClickButtonCreate()
-    {
-        $this->logIn('user', 'password');
-        $crawler = $this->client->request('GET', '/');
-
-        $this->assertContains('/tasks/create', $crawler->filter('a')->extract(['href']));
-
-        $link = $crawler->selectLink('Créer une nouvelle tâche')->link();
-        $crawler = $this->client->click($link);
-
-        $this->assertSame(2, $crawler->filter('input')->count());
-        $this->assertSame(1, $crawler->filter('html:contains("Retour à la liste des tâches")')->count());
     }
 
     public function testClickButtonListTasksToDo()
@@ -68,6 +55,95 @@ class TaskControllerTest extends WebTestCase
         self::assertContains('/tasks/create', $crawler->filter('a')->extract(['href']));
         self::assertContains('Liste des tâches complété', $crawler->filter('h1')->html());
         self::assertContains('/img/todolist_content.jpg', $crawler->filter('img')->extract(['src']));
+    }
+
+    public function testCreateTask()
+    {
+        $this->logIn('user', 'password');
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertContains('/tasks/create', $crawler->filter('a')->extract(['href']));
+
+        $link = $crawler->selectLink('Créer une nouvelle tâche')->link();
+        $crawler = $this->client->click($link);
+
+        $this->assertSame(2, $crawler->filter('input')->count());
+        $this->assertSame(1, $crawler->filter('html:contains("Retour à la liste des tâches")')->count());
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['task[title]'] = 'TaskCreated';
+        $form['task[content]'] = 'content';
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+
+        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
+        // verification de l'ajout
+        self::assertSame(1, $crawler->filter('html:contains("TaskCreated")')->count());
+        // présence du message alert
+        self::assertSame(1, $crawler->filter('html:contains("La tâche a été bien été ajoutée")')->count());
+    }
+
+    public function testDeleteTask()
+    {
+        $this->logIn('user', 'password');
+
+        $task = $this->getContainer()->get('doctrine')->getRepository(Task::class)->findOneByTitle('taskForDelete');
+
+        $crawler = $this->client->request('GET', '/tasks');
+
+        self::assertContains('/tasks/'.$task->getId().'/delete', $crawler->filter('a')->extract(['href']));
+
+        $this->client->request('GET', '/tasks/'.$task->getId().'/delete');
+        $crawler = $this->client->followRedirect();
+
+        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
+        self::assertSame(0, $crawler->filter('html:contains("taskForDelete")')->count());
+        self::assertSame(1, $crawler->filter('html:contains("La tâche a bien été supprimée.")')->count());
+    }
+
+    public function testEditTask()
+    {
+        $this->logIn('user', 'password');
+
+        $task = $this->getContainer()->get('doctrine')->getRepository(Task::class)->findOneByTitle('taskForEdit');
+
+        $crawler = $this->client->request('GET', '/tasks');
+
+        // présence du boutton de modification
+        self::assertContains('/tasks/'.$task->getId().'/edit', $crawler->filter('a')->extract(['href']));
+
+        $crawler =$this->client->request('GET', '/tasks/'.$task->getId().'/edit');
+
+        $form = $crawler->selectButton('Modifier la tâche')->form();
+        $form['task[title]'] = 'editedTask';
+        $form['task[content]'] = 'editedContent';
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+
+        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
+        // verification de l'édition
+        self::assertSame(1, $crawler->filter('html:contains("editedTask")')->count());
+        //présence du message alert
+        self::assertSame(1, $crawler->filter('html:contains("La tâche a bien été modifiée.")')->count());
+    }
+
+    public function testToggleTask()
+    {
+        $this->logIn('user', 'password');
+
+        $task = $this->getContainer()->get('doctrine')->getRepository(Task::class)->findOneByTitle('taskForToggle');
+
+        $crawler = $this->client->request('GET', '/tasks');
+        // présence du boutton de modification d'état
+        self::assertContains('/tasks/'.$task->getId().'/toggle', $crawler->filter('a')->extract(['href']));
+
+        //test de la non présence dans la liste to do
+        $crawler = $this->client->request('GET', '/tasks/'.$task->getId().'/toggle');
+        self::assertNotContains('/tasks/'.$task->getId().'/toggle', $crawler->filter('a')->extract(['href']));
+
+        //test présence de la tache dans la liste is done
+        $crawler = $this->client->request('GET', '/tasks/1');
+        self::assertContains('/tasks/'.$task->getId().'/toggle', $crawler->filter('a')->extract(['href']));
     }
 
     public function logIn($userType, $password)
